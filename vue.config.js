0512ea2634da237b7500b515fc7327e2d23ead33
iluxa <99901788+iluxaBoy@users.noreply.github.com>
2023-04-20 15:24:11 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  base: "/carusel/",
   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   transpileDependencies: true
 })
