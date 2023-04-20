064c950facc30a2e17440d588d5f17a2bcd32bbe
iluxaBoy <illapetefi@gmail.com>
2023-04-20 04:20:32 +0300

    some

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+  <div class="block1"></div>
+  <Carousel/>
+  <div class="block2"></div>
+</div>
+</template>
+
+<script>
+import Carousel from "./components/Carousel.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Carousel
+  }
+}
+</script>
+
+<style>
+*, *::before, *::after{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background-color: #111;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-align: center;
+  color: #b1d8ff;
+  margin-top: 60px;
+}
+.block1{
+  position: absolute;
+  height: 600px;
+  width: 350px;
+  top: 0;
+  background-image: linear-gradient(0.25turn, rgba(17, 17, 17, 0.4) , rgba(250,250,250,0) 100%);
+  z-index: 2;
+}
+.block2{
+  position: absolute;
+  height: 600px;
+  width: 350px;
+  top: 0;
+  right: 0;
+  background-image: linear-gradient(-0.25turn, rgba(17, 17, 17, 0.4), rgba(250,250,250,0) 100%);
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,201 @@
+<template>
+<div class="main">
+    <div class="container">
+        <div class="carousel">
+            <div class="cerved-up"></div>
+            <div class="inner" ref="inner" :style="innerStyles">
+                <div class="card" v-for="card in cards" :key="card" @click="getSrc(card)">
+                    <img :src="card" alt="img" >
+                </div>
+            </div>
+            <div class="cerved-down"></div>
+            <button class="btn-prev" @click="prev"></button>
+            <button class="btn-next" @click="next"></button>
+        </div>
+    </div>
+    <div :class="list">
+        <ol>
+            <li v-for="img in selectedImg" :key="img">
+                {{img}}
+            </li>
+        </ol>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+name:"CarouseL",
+data () {
+    return {
+    cards: [
+        "https://picsum.photos/id/237/640/370",
+        "https://picsum.photos/id/238/640/370",
+        "https://picsum.photos/id/239/640/370",
+        "https://picsum.photos/id/231/640/370",
+        "https://picsum.photos/id/232/640/370",
+        "https://picsum.photos/id/233/640/370",
+        "https://picsum.photos/id/234/640/370",
+        "https://picsum.photos/id/235/640/370",
+        "https://picsum.photos/id/236/640/370",
+    ],
+    innerStyles: {},
+    selectedImg: [],
+    step: '',
+    transitioning: false,
+    list: '',
+    }
+},
+mounted () {
+    this.setStep()
+    this.resetTranslate()
+},
+methods: {
+    setStep () {
+        const innerWidth = this.$refs.inner.scrollWidth
+        const totalCards = this.cards.length
+        this.step = `${innerWidth / totalCards}px`
+    },
+    next () {
+        if (this.transitioning) return
+        this.transitioning = true
+        this.moveLeft()
+        this.afterTransition(() => {
+            const card = this.cards.shift()
+            this.cards.push(card)
+            this.resetTranslate()
+            this.transitioning = false
+        })
+    },
+    prev () {
+        if (this.transitioning) return
+        this.transitioning = true
+        this.moveRight()
+        this.afterTransition(() => {
+            const card = this.cards.pop()
+            this.cards.unshift(card)
+            this.resetTranslate()
+            this.transitioning = false
+        })
+    },
+    moveLeft () {
+        this.innerStyles = {
+            transform: `translateX(-${this.step})
+                        translateX(-${this.step})`
+        }
+    },
+    moveRight () {
+        this.innerStyles = {
+            transform: `translateX(${this.step})
+                        translateX(-${this.step})`
+        }
+    },
+    afterTransition (callback) {
+        const listener = () => {
+            callback()
+            this.$refs.inner.removeEventListener('transitionend', listener)
+        }
+        this.$refs.inner.addEventListener('transitionend', listener)
+    },
+    resetTranslate () {
+        this.innerStyles = {
+            transition: 'none',
+            transform: `translateX(-${this.step})`
+        }
+    },
+    getSrc(card){
+        this.selectedImg.push(card)
+        this.list = 'list'
+    }
+}
+}
+</script>
+
+<style>
+.main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
+    padding-bottom: 50px;
+}
+.container{
+    position: relative;
+}
+.carousel {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 150px;
+    width: 1920px;
+    height: 400px;
+}
+.inner {
+    transition: transform 0.5s;
+    white-space: nowrap;
+}
+.card {
+    margin: 0 28px;
+    display: inline-flex;
+    color: white;
+    align-items: center;
+    justify-content: center;
+}
+.btn-prev {
+    position: absolute;
+    width: 31%;
+    height: 350px;
+    top: 170px;
+    left: 0;
+    /* background-color: inherit; */
+    border: none;
+    z-index: 5;
+}
+.btn-next {
+    position: absolute;
+    width: 31%;
+    height: 350px;
+    top: 170px;
+    right: 0;
+    /* background-color: inherit; */
+    border: none;
+    z-index: 5;
+}
+img {
+    width: 640px;
+    height: 370px;
+}
+ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.list{
+    padding: 40px 60px;
+    top: 555px;
+    background-color: #2e2e2e;
+    border-radius: 10px;
+    transition: 0.5s;
+    z-index: 16;
+}
+.cerved-up{
+  position: absolute;
+  height: 50px;
+  width: 2000px;
+  margin: auto;
+  top: 135px;
+  background-color: #111;
+  border-radius: 100%;
+  z-index: 5;
+}
+.cerved-down{
+  position: absolute;
+  height: 50px;
+  width: 2000px;
+  margin: auto;
+  bottom: -5px;
+  background-color: #111;
+  border-radius: 100%;
+  z-index: 5;
+}
+</style>
